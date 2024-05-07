58dc31272c7ae4b74be447a9e40b120d29588f30
VictorYan <victoryanks@outlook.com>
2024-05-07 10:24:22 +0800

    添加20个SingleMovie样例

---
+++ lib/examples.dart
@@ -0,0 +1,346 @@
+import 'utils/data_structure.dart';
+
+class Examples {
+  final SingleMovie _singleMovie1 = SingleMovie.fromJson({
+    'id': null,
+    'tmdbId': 823464,
+    'adult': 0,
+    'backdropPath': '/qrGtVFxaD8c7et0jUtaYhyTzzPg.jpg',
+    'originalLanguage': 'en',
+    'originalTitle': 'Godzilla x Kong: The New Empire',
+    'overview':
+        '继上一次的怪兽高燃对战之后，金刚和哥斯拉将再度联手对抗一个潜伏在世界中的巨大威胁，并逐步探索这些巨兽们的起源以及骷髅岛等地的奥秘。同时，上古之战的面纱也将会被揭晓，而正是那场战斗创造出了这些超凡的生物，并深刻影响了人类世界的命运。',
+    'popularity': 2229.531,
+    'posterPath': '/apO3t7Q5EvcFGDPNcW4duENjfWt.jpg',
+    'releaseDate': '2024-03-27',
+    'title': '哥斯拉大战金刚2：帝国崛起',
+    'voteAverage': 6.475,
+    'voteCount': 951
+  });
+  final SingleMovie _singleMovie2 = SingleMovie.fromJson({
+    'id': null,
+    'tmdbId': 1094844,
+    'adult': 0,
+    'backdropPath': '/jnE1GA7cGEfv5DJBoU2t4bZHaP4.jpg',
+    'originalLanguage': 'en',
+    'originalTitle': 'Ape vs. Mecha Ape',
+    'overview':
+        '认识到它的圈养巨猿的破坏力，军方制造了自己的战备人工智能——机甲猿。但是它的第一次实际测试出现了可怕的错误，使军方别无选择，只能释放被囚禁的巨猿来阻止它面前的巨型机器人',
+    'popularity': 2106.85,
+    'posterPath': '/dJaIw8OgACelojyV6YuVsOhtTLO.jpg',
+    'releaseDate': '2023-03-24',
+    'title': '猿猴大战机械猿猴',
+    'voteAverage': 5.113,
+    'voteCount': 111
+  });
+  final SingleMovie _singleMovie3 = SingleMovie.fromJson({
+    'id': null,
+    'tmdbId': 693134,
+    'adult': 0,
+    'backdropPath': '/xOMo8BRK7PfcJv9JCnx7s5hj0PX.jpg',
+    'originalLanguage': 'en',
+    'originalTitle': 'Dune: Part Two',
+    'overview':
+        '保罗·厄崔迪与契妮和弗雷曼人联手，踏上对致其家毁人亡的阴谋者的复仇之路。当面对一生挚爱和已知宇宙命运之间的抉择时，他必须努力阻止只有他能预见的可怕的未来。',
+    'popularity': 1753.806,
+    'posterPath': '/1p25wDEdFRRTtwtPFbtPHISefzG.jpg',
+    'releaseDate': '2024-02-27',
+    'title': '沙丘2',
+    'voteAverage': 8.217,
+    'voteCount': 3675
+  });
+  final SingleMovie _singleMovie4 = SingleMovie.fromJson({
+    'id': null,
+    'tmdbId': 1096197,
+    'adult': 0,
+    'backdropPath': '/4woSOUD0equAYzvwhWBHIJDCM88.jpg',
+    'originalLanguage': 'en',
+    'originalTitle': 'No Way Up',
+    'overview':
+        '一架满载游客的飞机起飞后突然起火，坠入太平洋深海中，惊魂未定的乘客被困机舱气囊中，氧气已所剩无几，大难不死的乘客以及机组人员正要开始庆祝，却遭到了几只嗜血鲨鱼的袭击，在深不见底的太平洋，鲨鱼群对他们展开凶猛的攻击，求生心切，幸存者们展开了人鲨大战。',
+    'popularity': 1746.101,
+    'posterPath': '/hu40Uxp9WtpL34jv3zyWLb5zEVY.jpg',
+    'releaseDate': '2024-01-18',
+    'title': '深渊鲨难',
+    'voteAverage': 6.372,
+    'voteCount': 595
+  });
+  final SingleMovie _singleMovie5 = SingleMovie.fromJson({
+    'id': null,
+    'tmdbId': 1011985,
+    'adult': 0,
+    'backdropPath': '/kYgQzzjNis5jJalYtIHgrom0gOx.jpg',
+    'originalLanguage': 'en',
+    'originalTitle': 'Kung Fu Panda 4',
+    'overview':
+        '神龙大侠阿宝再度归来，要被师父强行进阶修行。神秘莫测的魅影妖后可以幻化成每一个阿宝的昔日宿敌。此次阿宝又结识了小真等新伙伴，并将一同开启这场冒险旅程。',
+    'popularity': 1723.801,
+    'posterPath': '/v9sk7CPhDXJKSkQIegVSBQ5nJnV.jpg',
+    'releaseDate': '2024-03-02',
+    'title': '功夫熊猫4',
+    'voteAverage': 7.107,
+    'voteCount': 1515
+  });
+  final SingleMovie _singleMovie6 = SingleMovie.fromJson({
+    'id': null,
+    'tmdbId': 934632,
+    'adult': 0,
+    'backdropPath': '/tpiqEVTLRz2Mq7eLq5DT8jSrp71.jpg',
+    'originalLanguage': 'en',
+    'originalTitle': 'Rebel Moon - Part Two: The Scargiver',
+    'overview':
+        '延续第一部的史诗故事，柯拉（索菲亚·波多拉 Sofia Boutella 饰）与幸存的战士准备好豁出一切，与勇敢的碧草星居民并肩作战，即使村落不复往日和平，仍是对抗母星失败的人们寻得的新家园，这一切，就靠他们来守护。最终大战前夕，这群战士必须面对各自的过往，也揭露他们究竟为何而战。当叛军势力星火燎原，王国大军倾巢而出准备平息动乱，战友情谊益发坚定，英雄崭露头角，不朽传奇也于焉诞生。',
+    'popularity': 1558.065,
+    'posterPath': '/AmYPKBecC7uJ7WqFNUnZs8gZrVZ.jpg',
+    'releaseDate': '2024-04-19',
+    'title': '月球叛军2：烙印之人',
+    'voteAverage': 6.1,
+    'voteCount': 669
+  });
+  final SingleMovie _singleMovie7 = SingleMovie.fromJson({
+    'id': null,
+    'tmdbId': 940721,
+    'adult': 0,
+    'backdropPath': '/bWIIWhnaoWx3FTVXv6GkYDv3djL.jpg',
+    'originalLanguage': 'ja',
+    'originalTitle': 'ゴジラ-1.0',
+    'overview':
+        '由于战争而失去了一切的日本化为了焦土。虽然在战争中存活但却失去了双亲的敷岛浩一遇到了在变为一片废墟的日本坚强地生活下去的女性大石典子。仿佛是在追击打算复兴日本的在战争中幸存下来的人们一样，神秘的巨大怪兽哥斯拉出现了。面对拥有压倒性的力量的哥斯拉，人们逐步摸索对抗它的方法。',
+    'popularity': 1424.679,
+    'posterPath': '/slR4MmEuZotuozB3EKdmwlzhrYN.jpg',
+    'releaseDate': '2023-11-03',
+    'title': '哥斯拉-1.0',
+    'voteAverage': 7.837,
+    'voteCount': 728
+  });
+  final SingleMovie _singleMovie8 = SingleMovie.fromJson({
+    'id': null,
+    'tmdbId': 1041613,
+    'adult': 0,
+    'backdropPath': '/5Eip60UDiPLASyKjmHPMruggTc4.jpg',
+    'originalLanguage': 'en',
+    'originalTitle': 'Immaculate',
+    'overview':
+        '　塞西莉亚（西德妮·斯威尼 饰）是一个非常虔诚的修女，她来到意大利乡间修道院开始新生活，却在这个几乎全是女人的地方意外怀孕，原来这里隐藏着黑暗和可怕的秘密。',
+    'popularity': 1300.38,
+    'posterPath': '/1n47agnt9fYCCiq0Xkwb2NwimIA.jpg',
+    'releaseDate': '2024-03-20',
+    'title': '无瑕修女',
+    'voteAverage': 6.151,
+    'voteCount': 395
+  });
+  final SingleMovie _singleMovie9 = SingleMovie.fromJson({
+    'id': null,
+    'tmdbId': 821937,
+    'adult': 0,
+    'backdropPath': '/6a3aiSYNqABoV1Fq8n10LMOBxhH.jpg',
+    'originalLanguage': 'vi',
+    'originalTitle': '578: Phát Đạn Của Kẻ Điên',
+    'overview':
+        '''越南对诙谐而宏大的印尼惊悚片《突袭》（2011）做出了惊人的回应，在卡车、船只和自行车上上演了一场致命的武术盛宴、满负荷的紧张气氛和肾上腺素。这部精心编排的全动作惊悚片在华丽的地点切入了恶性复仇的最黑暗角落。
+  与前两部电影《父亲与儿子》和《Drowsy City》相比，Luong Derình Dũng进行了180度大转弯，展现了一个令人瞠目结舌的场面，包括大规模的打斗、激烈的追逐、剑斗、尖锐的伤口和曲折的陷阱。非时间顺序的情感倒叙为疯狂的愤怒、野蛮的愤怒、极端的折磨和不受约束的杀戮欲望提供了令人震惊、扣人心弦和似是而非的动机。
+  老实巴交的卡车司机洪得知，他十几岁的女儿安的抑郁和偏执是最近被绑架和猥亵造成的。这位慈爱的父亲愤怒地乘车向令人毛骨悚然的心理恋童癖百万富翁复仇，这位百万富翁受到他被宠坏、无情的音乐家儿子、可怜的顽固党羽和肮脏的荡妇女摩托车手的保护。这些混蛋应该在没有50/50或#metoo忏悔、情绪、内疚或怜悯的情况下被彻底清除。这里有一些惊人的东西可以在大屏幕上看到，人们对即将上映的续集的期望很高。
+  越南提名该片获得奥斯卡最佳国际影片奖。''',
+    'popularity': 1101.749,
+    'posterPath': '/cPTphSqJ0dF0EPeeAR1qeeNbMPV.jpg',
+    'releaseDate': '2022-05-20',
+    'title': '578：狂人一击',
+    'voteAverage': 5.16,
+    'voteCount': 25
+  });
+  final SingleMovie _singleMovie10 = SingleMovie.fromJson({
+    'id': null,
+    'tmdbId': 845111,
+    'adult': 0,
+    'backdropPath': '/qGJASuD3fs9ZBxuEZoxCLVidVq8.jpg',
+    'originalLanguage': 'fr',
+    'originalTitle': 'Les trois mousquetaires : Milady',
+    'overview':
+        '从卢浮宫到白金汉宫，到巴黎的阴沟到拉罗谢尔的围城……在一个被宗教战争分裂并受到英国入侵威胁的王国里，少数男男女女将剑拔弩张，将他们的命运与法国的命运绑在一起 。',
+    'popularity': 1093.541,
+    'posterPath': '/oxPdfTJLfCZauJiUdhHe5lA4bhD.jpg',
+    'releaseDate': '2023-12-13',
+    'title': '三个火枪手：米莱迪',
+    'voteAverage': 6.484,
+    'voteCount': 378
+  });
+  final SingleMovie _singleMovie11 = SingleMovie.fromJson({
+    'id': null,
+    'tmdbId': 748783,
+    'adult': 0,
+    'backdropPath': '/v5XyXZe8FADw8iHupB4L7QOAwH9.jpg',
+    'originalLanguage': 'en',
+    'originalTitle': 'The Garfield Movie',
+    'overview':
+        '马克·汀戴尔([四眼天鸡])将执导Alcon娱乐打造的全新CG动画片[加菲猫]。本片由保罗·卡普兰和马克·托格夫操刀剧本，约翰·可汗([神偷奶爸]、[愤怒的小鸟])等担任制片人。在原漫画中加菲猫是一只愤世嫉俗的可爱肥猫，其他角色还包括主人乔恩·阿佛克和宠物狗欧迪。该片的前期制作将于下月在洛杉矶开始。',
+    'popularity': 1091.629,
+    'posterPath': '/zK2sFxZcelHJRPVr242rxy5VK4T.jpg',
+    'releaseDate': '2024-04-30',
+    'title': '加菲猫',
+    'voteAverage': 6.3,
+    'voteCount': 34
+  });
+  final SingleMovie _singleMovie12 = SingleMovie.fromJson({
+    'id': null,
+    'tmdbId': 653346,
+    'adult': 0,
+    'backdropPath': '/fypydCipcWDKDTTCoPucBsdGYXW.jpg',
+    'originalLanguage': 'en',
+    'originalTitle': 'Kingdom of the Planet of the Apes',
+    'overview':
+        '“猿流感”病毒蔓延全球，人类几近毁灭，而进化为高级智慧生物的猿类一跃成为食物链顶端的王者。时移世异，暴虐的猿类首领赛撒在一片废墟中建立起猿族历史上的第一个帝国——“猩球”崛起，这是世界新秩序诞生的黎明时分。神秘的人类女孩诺娃的出现，引起了赛撒的警觉。她似乎掌握着一个巨大的秘密，不仅关乎人类的存亡，甚至可能再度颠覆整个“猩球”。被迫逃亡的诺娃在躲避赛撒军队的过程中，与青年猿类诺亚的命运交汇，揭开了一段冒险的旅程。',
+    'popularity': 990.656,
+    'posterPath': '/tE10WMV3SK2kq79aLHztJhz0EM5.jpg',
+    'releaseDate': '2024-05-08',
+    'title': '猩球崛起：新世界',
+    'voteAverage': 7.1,
+    'voteCount': 18
+  });
+  final SingleMovie _singleMovie13 = SingleMovie.fromJson({
+    'id': null,
+    'tmdbId': 976906,
+    'adult': 0,
+    'backdropPath': '/ilK1JRbMjo4sMJtNKLOKnqRf1RH.jpg',
+    'originalLanguage': 'en',
+    'originalTitle': 'Spitfire Over Berlin',
+    'overview':
+        '1944年8月。随着美国第八航空队准备打击纳粹德国，英国情报部门了解到他们可能会进入一个致命的陷阱。在仅剩几个小时的时间里，飞行中尉爱德华·巴恩斯必须驾驶着他手无寸铁的喷火战斗机在柏林上空执行一项生死攸关的任务，以获取照片证据，拯救1200人的生命⋯',
+    'popularity': 962.025,
+    'posterPath': '/9LvDkYj6JXGwRwpUrYU5uK0v8Kl.jpg',
+    'releaseDate': '2022-05-13',
+    'title': '喷火战斗机在柏林',
+    'voteAverage': 3.8,
+    'voteCount': 18
+  });
+  final SingleMovie _singleMovie14 = SingleMovie.fromJson({
+    'id': null,
+    'tmdbId': 1216299,
+    'adult': 0,
+    'backdropPath': '/v8RgWCgabyym0eVOuQJaxL4GS8p.jpg',
+    'originalLanguage': 'en',
+    'originalTitle': 'The Night They Came Home',
+    'overview': '',
+    'popularity': 934.279,
+    'posterPath': '/6Bd17axAG0qJ6YIU3SoootXV0Cz.jpg',
+    'releaseDate': '2024-01-12',
+    'title': '他们回家的那晚',
+    'voteAverage': 5.5,
+    'voteCount': 13
+  });
+  final SingleMovie _singleMovie15 = SingleMovie.fromJson({
+    'id': null,
+    'tmdbId': 601796,
+    'adult': 0,
+    'backdropPath': '/7ZP8HtgOIDaBs12krXgUIygqEsy.jpg',
+    'originalLanguage': 'ko',
+    'originalTitle': '외계+인 1부',
+    'overview': '该片讲述了道士们争夺传闻中神剑的高丽末期和外星人出没的2022年之间的时光之门被打开后，引发的异想天开的故事。',
+    'popularity': 909.24,
+    'posterPath': '/hSb1ky8t1sb96gJtoqaBuT4Xln6.jpg',
+    'releaseDate': '2022-07-20',
+    'title': '外星+人',
+    'voteAverage': 6.821,
+    'voteCount': 312
+  });
+  final SingleMovie _singleMovie16 = SingleMovie.fromJson({
+    'id': null,
+    'tmdbId': 784651,
+    'adult': 0,
+    'backdropPath': '/keVfqCMKmJ55nHzmqR2Q5K7LwJt.jpg',
+    'originalLanguage': 'hi',
+    'originalTitle': 'फाइटर',
+    'overview':
+        '面对迫在眉睫的危险，印度空军的顶级飞行员齐聚一堂，组建了“空中龙”。 《战士》展现了他们的友情、兄弟情谊以及内部和外部的战斗。',
+    'popularity': 884.768,
+    'posterPath': '/18mTWwMBZZo69DeddU3xUE1tQkJ.jpg',
+    'releaseDate': '2024-01-24',
+    'title': '战士',
+    'voteAverage': 5.287,
+    'voteCount': 68
+  });
+  final SingleMovie _singleMovie17 = SingleMovie.fromJson({
+    'id': null,
+    'tmdbId': 940551,
+    'adult': 0,
+    'backdropPath': '/2KGxQFV9Wp1MshPBf8BuqWUgVAz.jpg',
+    'originalLanguage': 'en',
+    'originalTitle': 'Migration',
+    'overview': '飞鸭马拉德一家向南迁徙前往热带的牙买加，却意外闯入疯狂大都市纽约？在紧张刺激的飞行之旅中，他们将如何化险为夷？',
+    'popularity': 882.885,
+    'posterPath': '/t6LAjw9C2fGYZy7zLmtEkcnC5UM.jpg',
+    'releaseDate': '2023-12-06',
+    'title': '飞鸭向前冲',
+    'voteAverage': 7.48,
+    'voteCount': 1294
+  });
+  final SingleMovie _singleMovie18 = SingleMovie.fromJson({
+    'id': null,
+    'tmdbId': 850888,
+    'adult': 0,
+    'backdropPath': '/AuTfo1lbE8s2Y8RVTmlbJAeLWPK.jpg',
+    'originalLanguage': 'es',
+    'originalTitle': 'Sayen: La cazadora',
+    'overview': '',
+    'popularity': 877.516,
+    'posterPath': '/btmngK5iXaxyAEl0ScIWCQG2ITr.jpg',
+    'releaseDate': '2024-04-26',
+    'title': 'Sayen: La cazadora',
+    'voteAverage': 6.6,
+    'voteCount': 18
+  });
+  final SingleMovie _singleMovie19 = SingleMovie.fromJson({
+    'id': null,
+    'tmdbId': 920342,
+    'adult': 0,
+    'backdropPath': '/7kQvHmGyKJv2wSKVfCUux50rd7A.jpg',
+    'originalLanguage': 'zh',
+    'originalTitle': '异兽战场',
+    'overview':
+        '　　兵王秦阳未婚妻叶沁遭遇未知巨兽惨死。顾平邀请他参与叶沁生前科研。但顾平却在利用叶沁的研究成果背地里结合未知巨兽的基因，造出“零号”龙形生物。富有智慧的龙形生物，再加上靠吞噬进化的域外巨兽，城市大战，一触即发……',
+    'popularity': 851.644,
+    'posterPath': '/nBVYp2xxx2R02n21EGlDky8CgWR.jpg',
+    'releaseDate': '2021-12-27',
+    'title': '异兽战场',
+    'voteAverage': 3.0,
+    'voteCount': 1
+  });
+  final SingleMovie _singleMovie20 = SingleMovie.fromJson({
+    'id': null,
+    'tmdbId': 1063879,
+    'adult': 0,
+    'backdropPath': '/k0ucFBBgSDTXYU8fVHXJyjAuIIe.jpg',
+    'originalLanguage': 'fr',
+    'originalTitle': 'Vermines',
+    'overview':
+        '孤独青年在住宅大楼自设昆虫馆，把各种昆虫和爬虫类视为宠物，蝎子蜈蚣爱不释手。新宠儿是一只外来蜘蛛，暂时给它一个安乐窝，只是掌上明蛛求生欲强，它要逃离禁室，不断繁衍。后代变种夺命巨蛛，大楼到处结满蛛网，居民陷入一片恐慌。基于公众安全理由，警方封锁现场所有出口，死里逃生更是难上加难。凡尼切克首部长片，直视无孔不入的致命威胁，触及社会的阶级歧视，勾起封城恐惧，善用特效与场景空间，拍出令人毛骨悚然血压狂飚的蛛杀危机。',
+    'popularity': 837.471,
+    'posterPath': '/pS7Z3SJUiyu2NzqPISFENYFY63b.jpg',
+    'releaseDate': '2023-12-27',
+    'title': '害虫',
+    'voteAverage': 6.955,
+    'voteCount': 168
+  });
+
+  List<SingleMovie>get singleMovieExamples => [
+    _singleMovie1,
+    _singleMovie2,
+    _singleMovie3,
+    _singleMovie4,
+    _singleMovie5,
+    _singleMovie6,
+    _singleMovie7,
+    _singleMovie8,
+    _singleMovie9,
+    _singleMovie10,
+    _singleMovie11,
+    _singleMovie12,
+    _singleMovie13,
+    _singleMovie14,
+    _singleMovie15,
+    _singleMovie16,
+    _singleMovie17,
+    _singleMovie18,
+    _singleMovie19,
+    _singleMovie20,
+  ];
+}
